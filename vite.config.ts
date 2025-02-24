b812fe5556a9ed095bab177233b2d3424ad2f6b4
Mohammed Fawzey <139973567+devfawzey@users.noreply.github.com>
2025-02-24 15:18:11 +0200

    UI: add tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,18 @@
-import { fileURLToPath, URL } from "node:url";
+import {fileURLToPath, URL} from "node:url";
 
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
 import Components from "unplugin-vue-components/vite";
 import AutoImport from "unplugin-auto-import/vite";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    tailwindcss(),
     Components({}),
     AutoImport({
       imports: [
